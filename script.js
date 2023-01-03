19c1618a587022acf2d6e3c1c089d069ff0efc3e
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-01-03 16:30:49 +0000

    create basic layout with HTML and CSS

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const myLibrary = [];
+const form = document.querySelector("form");
 const submit = document.querySelector("button");
 const newTitle = document.getElementById("title");
 const newAuthor = document.getElementById("author");
+const newPublished = document.getElementById("published");
 const newPages = document.getElementById("pages");
 const newHasRead = document.getElementById("has-read");
 
-function Book(title, author, pages, hasRead) {
+function Book(title, author, published, pages, hasRead) {
   this.title = title;
   this.author = author;
+  this.published = published;
   this.pages = pages;
   this.hasRead = hasRead;
-}
-
-function clearInputValues() {
-  newTitle.value = "";
-  newAuthor.value = "";
-  newPages.value = "";
-  newHasRead.value = "";
 }
 
 function addBookToLibrary(event) {
@@ -24,12 +20,13 @@ function addBookToLibrary(event) {
   const newBook = new Book(
     newTitle.value,
     newAuthor.value,
+    newPublished.value,
     newPages.value,
     newHasRead.checked ? newHasRead.value : "Off"
   );
   myLibrary.push(newBook);
   console.log(myLibrary);
-  clearInputValues();
+  form.reset();
 }
 
 submit.addEventListener("click", addBookToLibrary);
