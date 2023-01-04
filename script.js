7e71dd1170e972d954d36bd6dcceb43a17fab269
Andrew <115462877+parkypark92@users.noreply.github.com>
2023-01-04 18:35:26 +0000

    change displayBooks function to only create current entry and display it instead of looping through array

--- script.js
+++ script.js
@@ -16,14 +16,11 @@ function Book(title, author, published, pages, hasRead) {
   this.hasRead = hasRead;
 }
 
-function displayBooks(array) {
-  bookShelf.textContent = "";
-  array.forEach((element) => {
-    const currentBook = document.createElement("div");
-    currentBook.classList.add("book");
-    currentBook.textContent = element.title;
-    bookShelf.appendChild(currentBook);
-  });
+function displayBooks(bookToDisplay) {
+  const currentBook = document.createElement("div");
+  currentBook.classList.add("book");
+  currentBook.textContent = bookToDisplay.title;
+  bookShelf.appendChild(currentBook);
 }
 
 function addBookToLibrary(event) {
@@ -33,31 +30,39 @@ function addBookToLibrary(event) {
     newAuthor.value,
     newPublished.value,
     newPages.value,
-    newHasRead.checked ? newHasRead.value : "Off"
+    newHasRead.checked ? newHasRead.value : "False"
   );
   myLibrary.push(newBook);
-  displayBooks(myLibrary);
+  displayBooks(newBook);
   form.reset();
 }
 
 submit.addEventListener("click", addBookToLibrary);
 
-// const testBook = new Book("Test Book1", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook);
-// const testBook2 = new Book("Test Book2", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook2);
-// const testBook3 = new Book("Test Book3", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook3);
-// const testBook4 = new Book("Test Book4", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook4);
-// const testBook5 = new Book("Test Book5", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook5);
-// const testBook6 = new Book("Test Book6", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook6);
-// const testBook7 = new Book("Test Book7", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook7);
-// const testBook8 = new Book("Test Book8", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook8);
-// const testBook9 = new Book("Test Book9", "Andrew Park", "2023", "300", "Off");
-// myLibrary.push(testBook9);
-// displayBooks(myLibrary);
+const testBook = new Book("Test Book1", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook);
+displayBooks(testBook);
+const testBook2 = new Book("Test Book2", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook2);
+displayBooks(testBook2);
+const testBook3 = new Book("Test Book3", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook3);
+displayBooks(testBook3);
+const testBook4 = new Book("Test Book4", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook4);
+displayBooks(testBook4);
+const testBook5 = new Book("Test Book5", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook5);
+displayBooks(testBook5);
+const testBook6 = new Book("Test Book6", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook6);
+displayBooks(testBook6);
+const testBook7 = new Book("Test Book7", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook7);
+displayBooks(testBook7);
+const testBook8 = new Book("Test Book8", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook8);
+displayBooks(testBook8);
+const testBook9 = new Book("Test Book9", "Andrew Park", "2023", "300", "Off");
+myLibrary.push(testBook9);
+displayBooks(testBook9);
